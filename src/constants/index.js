b89cc095064f68c590282dac00ca20864ccca466
Sabry Dawood <72863390+virgel1995@users.noreply.github.com>
2023-02-28 17:15:30 +0000

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+
+const domain = "https://store-api.virgel1995.repl.co"
+
+
+export const base_url = domain;
---
+++ src/containers/ControlPanel/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class ControlPanel extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className= "text-danger">ControlPanel</h2>
+            </div>
+        );
+    }
+}
+
+export default ControlPanel;
